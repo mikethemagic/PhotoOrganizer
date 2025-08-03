a4b176e4b7dd955e6852b928e742ad73cf261797
mistangl <michael.stangl@posteo.de>
2025-08-03 07:20:53 +0200

    config dazu genommen. setenv etwas erweitert

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -3,13 +3,15 @@
 export PROJECT=/mnt/c/10-Develop/gitrepos/PhotoOrganiser
 export PROJECT_BIN=$PROJECT/bin
 export PROJECT_DATA=$PROJECT/data
-export PROJECT_WORK=$PROJECT/work
+export PROJECT_SCRIPTS=$PROJECT/scripts
+export PROJECT_CACHE=$PROJECT/cache
 export PROJECT_LIB=$PROJECT/lib
 
 
 # Create directories if they don't exist
-mkdir -p "$PROJECT/work"
+mkdir -p "$PROJECT/scripts"
 mkdir -p "$PROJECT/data"
+mkdir -p "$PROJECT/cache"
 
 # Add project bin to PATH
 export PATH="$PROJECT_BIN:$PATH"
